61a850a4ae472cb9835614e448da08ce52df6e52
t3rmin <terminationprompt@gmail.com>
2026-07-16 12:29:07 -0500

    Update main.js

--- chrome-dino/js/main.js
+++ chrome-dino/js/main.js
@@ -4,5 +4,5 @@ let dino = new Dino(canvas, {
     maxWidth: 720
 });
 
-// Let init handle loading the image AND automatically starting the loop when safe!
+// This opens the engine and loads assets safely!
 dino.init(); 
