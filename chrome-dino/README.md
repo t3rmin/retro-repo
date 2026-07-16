973abcfb33dfc6aaba40eafcc352fbcb21755a9b
t3rmin <terminationprompt@gmail.com>
2026-07-16 12:38:40 -0500

    Delete chrome-dino directory

--- chrome-dino/README.md
+++
@@ -1,3 +0,0 @@
-# Dinosaur T-Rex Game
-Chrome no internet T-Rex game
-![Screenshot](https://raw.githubusercontent.com/zuramai/chrome-dino-game/main/screenshot.png)
--- chrome-dino/js/main.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById('canvas');
-
-let dino = new Dino(canvas, {
-    maxWidth: 720
-});
-
-// This opens the engine and loads assets safely!
-dino.init(); 
